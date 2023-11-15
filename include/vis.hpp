406bc18162eb930ae7909e3eb937a1cc042debee
ZHANG, Meng <stall-breezes.0t@icloud.com>
2023-11-15 18:34:47 +1300

    Can Eliminate Lines

--- include/vis.hpp
+++ include/vis.hpp
@@ -35,6 +35,42 @@ namespace Vis {
         }
     }
 
+    // check if a row is full
+    static inline bool CheckRowFull(const Pixels &board, const uint8_t row) {
+        // check if the row is full
+        return std::all_of(board.begin() + row * WIDTH,
+                           board.begin() + (row + 1) * WIDTH,
+                           [](const auto &i) { return i != Canvas::Key::NUL; });
+    }
+
+    // remove a row from the board
+    //
+    // 1. shift above rows down
+    // 2. set the top row to empty
+    static inline void RmRow(Pixels &board, const uint8_t row) {
+        // shift by std::copy rows [0, row) to rows [1, row + 1)
+        std::copy(board.begin(), board.begin() + row * WIDTH,
+                  board.begin() + WIDTH);
+        // set the top row to empty
+        std::fill(board.begin(), board.begin() + WIDTH, Canvas::Key::NUL);
+    }
+
+    // check for full rows and remove them
+    static inline uint16_t RowsExplode(Pixels &board) {
+        uint16_t count = 0;
+        // check for full rows
+        for (uint8_t row = 0; row < HEIGHT; ++row) {
+            // check if the row is full
+            if (CheckRowFull(board, row)) {
+                // remove the row
+                RmRow(board, row);
+                // increment the score
+                ++count;
+            }
+        }
+        return count;
+    }
+
     // visualize the board
     void ToStr(const Pixels &, const Dict &, str &);
 
