717f4ecfe59020574c5d62b67d86e341fb00afa5
ZHANG, Meng <stall-breezes.0t@icloud.com>
2023-11-15 21:38:09 +1300

    Fixes and Revise Game Play
    - fixes
      - indices always follow "top-down, left-right" order
      - fix rotation indices
    - arrange display by `Screen` (array of strings)
    - add `last_fall` to `GameContext`
    - add pause and quit
    - revise UI
    - re-org

--- include/term.hpp
+++ include/term.hpp
@@ -1,19 +1,20 @@
 #pragma once
 
+#include "config.h"
+#include "consts.hpp"
+#include "types.hpp"
+
 #include <fcntl.h>
-#include <map>
+#include <iomanip>
+#include <iostream>
 #include <termios.h>
 #include <unistd.h>
-
-typedef std::chrono::seconds sec;
-typedef std::chrono::steady_clock steady_clock;
-typedef std::chrono::time_point<steady_clock> time_point;
 
 namespace Term {
 
     using Termios = struct termios;
 
-    inline void enableRawMode(Termios &orig_termios) {
+    static inline void enableRawMode(Termios &orig_termios) {
         tcgetattr(STDIN_FILENO, &orig_termios);
         struct termios raw = orig_termios;
         raw.c_lflag &= ~(ECHO | ICANON);
@@ -24,8 +25,41 @@ namespace Term {
         fcntl(STDIN_FILENO, F_SETFL, flags | O_NONBLOCK);
     }
 
-    inline void disableRawMode(const Termios &orig_termios) {
+    static inline void disableRawMode(const Termios &orig_termios) {
         tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    }
+
+    // Clear the page and move cursor to home position
+    static inline void clearScreen() { std::cout << "\033[2J\033[1;1H"; }
+
+    static inline void printScreen(const Screen &board, const uint16_t &s,
+                                   const bool &game_over) {
+        // add top border
+        std::cout << std::string(42, '#') << std::endl;
+        for (uint8_t i = 0; i < board.size(); ++i) {
+            // add left separator
+            std::cout << "##";
+            // print line
+            std::cout << board[i];
+            // add right separator
+            std::cout << "##";
+            if (i == 2) {
+                std::cout << " VERSION: " << VERSION_MAJOR << "."
+                          << VERSION_MINOR << "." << VERSION_PATCH << " "
+                          << "##";
+            } else if (i == 4) {
+                std::cout << "  SCORE: " << std::setw(5) << s << "  "
+                          << "##";
+            } else if (i == 6 && game_over) {
+                std::cout << "    GAME OVER   "
+                          << "##";
+            } else {
+                std::cout << std::string(16, ' ') << "##";
+            }
+            std::cout << std::endl;
+        }
+        // add bottom border
+        std::cout << std::string(42, '#') << std::endl;
     }
 
 } // namespace Term
--- src/main.cpp
+++ src/main.cpp
@@ -1,94 +1,85 @@
+#include "consts.hpp"
 #include "move.hpp"
 #include "term.hpp"
-#include "tetris.hpp"
+#include "types.hpp"
 #include "vis.hpp"
 #include <iostream>
 
-// validate
-bool Valid(const Indices &buffer, const Pixels &board) {
-    // out of bound
-    for (const auto &i : buffer) {
-        if (i == MAX) {
-            return false;
-        }
-    }
-    // collision detection
-    for (const auto &i : buffer) {
-        if (board[i] != Canvas::Key::NUL) {
-            return false;
-        }
-    }
-    return true;
-}
-
-void autoDown(PieceContext &piece, GameContext &game, time_point &lastFall) {
-    if (duration_cast<sec>(steady_clock::now() - lastFall).count() >= 1) {
-        lastFall = steady_clock::now();
+// game logic of auto falling
+void Fall(PieceContext &piece, GameContext &game, bool &game_over) {
+    if (duration_cast<sec>(steady_clock::now() - game.last_fall).count() >= 1) {
+        game.last_fall = steady_clock::now();
         // move down if possible
         // add to board otherwise
-        if (Valid(piece.down, game.base)) {
+        if (Move::Valid(piece.down, game.base)) {
             Move::Down(piece);
         } else {
-            // add to board
-            uint8_t idx = Vis::Reload(game);
-            // check for full rows
-            uint16_t rows = Vis::RowsExplode(game.active);
-            // update to base board
-            game.base = game.active;
-            // update score
-            game.lines += rows;
-            // spawn new piece
-            Move::Init(idx, piece);
+            // explode full rows
+            Vis::RowsExplode(game);
+            // initiate new piece
+            Move::Spawn(Vis::Pop(game), piece);
+            // check if the game is over
+            if (!Move::Valid(piece.down, game.base)) {
+                game_over = true;
+            }
         }
     }
 }
 
 int main() {
-    GameContext game = Vis::InitGame();
-    PieceContext piece;
-    uint8_t idx = Vis::Reload(game);
-    Move::Init(idx, piece);
-
-    // Initialize the board
-    std::string screen;
-
+    // Enable raw mode
     Term::Termios orig_termios;
     Term::enableRawMode(orig_termios);
 
-    time_point lastFall = steady_clock::now(); // Last time the piece moved down
+    // Initialize the board
+    bool game_over = false;
+    Screen board;
+    char ctrl;
 
-    while (true) {
-        // Game logic for moving down
-        autoDown(piece, game, lastFall);
+    GameContext game = Vis::InitGame();
+    PieceContext piece;
+    Move::Spawn(Vis::Pop(game), piece);
 
-        char inputChar;
-        if (read(STDIN_FILENO, &inputChar, 1) > 0) {
-            if (inputChar == 'h' && Valid(piece.left, game.base)) {
+    while (!game_over) {
+        Fall(piece, game, game_over);
+
+        if (read(STDIN_FILENO, &ctrl, 1) > 0) {
+            if (ctrl == 'h' && Move::Valid(piece.left, game.base)) {
                 Move::Left(piece);
-            }
-            if (inputChar == 'l' && Valid(piece.right, game.base)) {
+            } else if (ctrl == 'l' && Move::Valid(piece.right, game.base)) {
                 Move::Right(piece);
-            }
-            if (inputChar == 'k' && Valid(piece.rotate, game.base) &&
-                Valid(piece.round, game.base)) {
+            } else if (ctrl == 'k' && Move::Valid(piece.rotate, game.base) &&
+                       Move::Valid(piece.round, game.base)) {
                 Move::Rotate(piece);
-            }
-            if (inputChar == 'j' && Valid(piece.down, game.base)) {
+            } else if (ctrl == 'j' && Move::Valid(piece.down, game.base)) {
                 Move::Down(piece);
+            } else if (ctrl == 'p') { // pause by pressing 'p'
+                while (true) {
+                    if (read(STDIN_FILENO, &ctrl, 1) > 0 && ctrl == 'p') {
+                        break;
+                    } else {
+                        usleep(10000);
+                    }
+                }
+            } else if (ctrl == 'q') { // quit by pressing 'q'
+                game_over = true;
+            } else { // otherwise beep
+                std::cout << "\a";
             }
         }
 
         // fill the board with blocks
         Vis::UpdateBoard(piece, game);
+        Vis::ToStr(game.active, Canvas::MAP, board);
 
-        // Clear the page and move cursor to home position
-        std::cout << "\033[2J\033[1;1H";
+        // clear the screen
+        Term::clearScreen();
 
-        Vis::ToStr(game.active, Canvas::MAP, screen);
-        std::cout << screen << std::endl;
+        // print the board
+        Term::printScreen(board, game.lines, game_over);
 
         // Optional: Add a short delay for smoother animation
-        usleep(10000); // 50 milliseconds
+        usleep(50000); // 50 milliseconds
     }
 
     Term::disableRawMode(orig_termios);
